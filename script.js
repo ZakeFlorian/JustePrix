564ef83f2d5af1ff487a3baee2ae4dc43b3b0749
Florian <stephe76bis4@outlook.fr>
2023-10-16 01:13:34 +0200

    ajout des images et prix random mais bug des tentatives

--- script.js
+++ script.js
@@ -1,38 +1,54 @@
-const name = ['Une chaise gaming (sans clé USB)', 'Un costume de Halloween', 'Un BBQ', 'Une guitare', 'Un sac à main']
-const article = ['chaise.png', 'costume-halloween.png', 'grill.png', 'guitare.png', 'sac-a-main.png']
-let inputPrix
-let Prix 
-let Random
-let nbreAttempts
-let showAttempts = document.getElementById('attempts')
-let bouton = document.getElementById('button')
-let imageName = document.getElementById('name')
-let image = document.getElementById('article')
-let message = document.getElementById('message')
+const nameImage = ['Une chaise gaming (sans clé USB)', 'Un costume de Halloween', 'Un BBQ', 'Une guitare', 'Un sac à main'];
+const article = ['chaise.png', 'costume-halloween.png', 'grill.png', 'guitare.png', 'sac-a-main.png'];
 
-nbreAttempts = 10
-showAttempts.innerHTML = "Il vous reste toutes vos tentatives ("+nbreAttempts+" restantes) !"
-function verifTentative(){
-    inputPrix = docuement.getElementById('inputPrix')
-    if(nbreAttempts == 0){
-        showAttempts.innerHTML = "Il vous reste"+nbreAttempts+"tentatives !"
-        message.innerHTML = "Vous avez perdu !! L'article était à"+Random+"€"
-        button.disabled = true
+let inputPrix;
+let Prix;
+let Random;
+let nbreAttempts;
+let showAttempts = document.getElementById('attempts');
+let button = document.getElementById('button');
+let imageName = document.getElementById('name');
+let articleImage = document.getElementById('article');
+let message = document.getElementById('message');
+
+function prixRandom(valeurMax) {
+    return Math.floor(Math.random() * valeurMax);
+}
+
+Random = prixRandom(5); // Sélection aléatoire d'un objet (0 à 4)
+Prix = prixRandom(100) + 1; // Sélection aléatoire d'un prix (1 à 100)
+
+function imageRandom(valeur) {
+    return '<img src="../JustePrix/img/' + valeur + '" class="img-fluid" width="30%">';
+}
+
+articleImage.innerHTML = imageRandom(article[Random]);
+imageName.innerHTML = nameImage[Random];
+
+nbreAttempts = 10;
+showAttempts.innerHTML = "Il vous reste " + nbreAttempts + " tentatives !";
+
+function verifTentative() {
+    inputPrix = document.getElementById('inputPrix').value;
+
+    if (nbreAttempts === 0) {
+        showAttempts.innerHTML = "Il vous reste " + nbreAttempts + " tentatives !";
+        message.innerHTML = "Vous avez perdu !! L'article était à " + Prix + "€";
+        button.disabled = true;
     } else {
-        if(inputPrix > Prix){
-            message.innerHTML = "Moins cher !"
-            nbreAttempts-1
-            showAttempts.innerHTML = "Il ne vous reste plus que"+nbreAttempts+"tentatives !"
-        }
-        if(inputPrix < Prix){
-            message.innerHTML = "Plus cher !"
-            nbreAttempts-1
-            showAttempts.innerHTML = "Il ne vous reste plus que"+nbreAttempts+"tentatives !"
-        }
-        if(inputPrix == Prix){
-            message.innerHTML = "C'est le juste prix !!!"
-            showAttempts.innerHTML = "Il ne vous restait plus que"+nbreAttempts+"tentatives !"
-            button.disabled = true
+        nbreAttempts--;
+        showAttempts.innerHTML = "Il ne vous reste plus que " + nbreAttempts + " tentatives !";
+
+        if (inputPrix > Prix) {
+            message.innerHTML = "Moins cher !";
+        } else if (inputPrix < Prix) {
+            message.innerHTML = "Plus cher !";
+        } else {
+            message.innerHTML = "C'est le juste prix !!!";
+            showAttempts.innerHTML = "Il ne vous restait plus que " + nbreAttempts + " tentatives !";
+            button.disabled = true;
         }
     }
 }
+
+button.addEventListener('click', verifTentative, false);
